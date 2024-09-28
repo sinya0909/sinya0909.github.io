a3259d7551a3205e5f710fe5cfc87f9e701f4428
yoshida <yoshida@polaris-inc.co.jp>
2024-09-28 12:05:42 +0900

    修正

--- service_worker.js
+++ service_worker.js
@@ -1,17 +1,38 @@
 // キャッシュファイルの指定
 var CACHE_NAME = 'pwa-sample-caches';
 var urlsToCache = [
+    'https://ancient-earth-54146-ad024a0db8ed.herokuapp.com/',
     '/sinya0909.github.io/',
+    '/sinya0909.github.io/test-2/',
 ];
 
+
+
 // インストール処理
+// self.addEventListener('install', function(event) {
+//     event.waitUntil(
+//         caches
+//             .open(CACHE_NAME)
+//             .then(function(cache) {
+//                 return cache.addAll(urlsToCache);
+
+//             })
+//     );
+// });
+
 self.addEventListener('install', function(event) {
     event.waitUntil(
-        caches
-            .open(CACHE_NAME)
-            .then(function(cache) {
-                return cache.addAll(urlsToCache);
-            })
+        (async function() {
+            const cache = await caches.open(CACHE_NAME);
+            for (const url of urlsToCache) {
+                try {
+                    await cache.add(url);
+                } catch (error) {
+                    console.error(`Failed to cache ${url}:`, error);
+                    // 次のURLに移動
+                }
+            }
+        })()
     );
 });
 
